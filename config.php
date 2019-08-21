fdda6eb44e38f42eb5d0da1ee822df08d8c580cf
Bayu Riza alfarizi <32736177+devikamenor@users.noreply.github.com>
2019-08-22 02:13:31 +0700

    Flash Go

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="ShadyDRX";
+
+//user_google_ad_id wajib isi
+$user_google_ad_id="e5ecd279-d68a-42e8-9bac-5b2a9ad8a358";
+//user_token wajib di isi //
+$user_token="Aby-MdZz7dlJ1yII";
+
+//user_id wajib di isi //
+$user_id="Facebook_650747498751351";
+
+// invite_code wajib di isi //
+$invite_code="F2ymm1j6FL";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va182NTA3NDc0OTg3NTEzNTEiLCJleHAiOiIxNTY5MDAxMzg4ODA1In0.mrsoyWBxNzkUS1HMDlGgNVo0KlIlHKCsmVjpI6YDcoU";
+
+
+
+?>
